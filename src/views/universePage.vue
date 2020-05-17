32f122bce6c012089c1bcb172a2bb1053389bf67
sbh-web <1950498286@qq.com>
2020-05-17 13:06:34 +0800

    修改了下bug

--- src/views/universePage.vue
+++ src/views/universePage.vue
@@ -4,10 +4,10 @@
             <swiper-slide class="univItem" v-for="(item, index) in swiper1Data" v-bind:key='index'>
                 <div :class="currentIndex == index?'showCon':''">
                     <p class="univName0">{{item.universeName}}</p>
-                    <p class="univDes">{{item.description"}}</p>
+                    <p class="univDes">{{item.description}}</p>
                 </div>
                 <div class="univImg">
-                    <router-link to="/universe-list?univId={{item.universeId}}"><img :src="item.image" alt=""></router-link>
+                    <router-link to="/universe-list?univId=item.universeId"><img :src="item.image" alt=""></router-link>
                     <p class="univName" :class="currentIndex == index?'hideCon':''">{{item.universeName}}</p>
                 </div>
             </swiper-slide>
@@ -28,8 +28,8 @@
 </template>
 
 <script>
-import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
-import 'swiper/css/swiper.css'
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper';
+import 'swiper/css/swiper.css';
 export default {
     data() {
       return {
@@ -88,40 +88,42 @@ export default {
         SwiperSlide
     },
     created() {
-        if(this.swiper1Data.length > 7){
-            this.swiperPag.slidesPerView = 7;
-        }else(
-            this.swiperPag.slidesPerView = this.swiper1Data.length
-        )
+        console.log("universePage-created");
+        // if(this.swiper1Data.length > 7){
+        //     this.swiperPag.slidesPerView = 7;
+        // }else(
+        //     this.swiperPag.slidesPerView = this.swiper1Data.length
+        // )
     },
     mounted(){
-         //在用户看到界面之前执行
-             var a = document.getElementById("tablebox");
-             var scroll_width = 100; //滚动一下的距离
-             if(document.addEventListener){
-                 document.addEventListener('DOMMouseScroll', mousewheel_event, false); // FF
-             }
-             a.onmousewheel = mousewheel_event; // IE/Opera/Chrome
-             function mousewheel_event(eee) {
-                 var eee2 = eee || window.event, v;
-                 eee2.wheelDelta ? v=eee2.wheelDelta : v=eee2.detail;
-                 if(v>3||-v>3) v=-v;
-                 v>0 ? a.scrollLeft+=scroll_width : a.scrollLeft-=scroll_width;
+        //在用户看到界面之前执行
+        console.log("universePage-mounted");
+        // var a = document.getElementById("tablebox");
+        // var scroll_width = 100; //滚动一下的距离
+        // if(document.addEventListener){
+        //     document.addEventListener('DOMMouseScroll', mousewheel_event, false); // FF
+        // }
+        // a.onmousewheel = mousewheel_event; // IE/Opera/Chrome
+        // function mousewheel_event(eee) {
+        //     var eee2 = eee || window.event, v;
+        //     eee2.wheelDelta ? v=eee2.wheelDelta : v=eee2.detail;
+        //     if(v>3||-v>3) v=-v;
+        //     v>0 ? a.scrollLeft+=scroll_width : a.scrollLeft-=scroll_width;
+        //
+        //     eee2.preventDefault(); //阻止浏览器的默认滚动
+        // }
 
-                 eee2.preventDefault(); //阻止浏览器的默认滚动
-             }
-
-             this.$api.get('api/main/pub/all', {
-             }, response =>{
-                 if (response.status >= 200 && response.status < 300) {
-                     console.log(response.data);
-                     this.resCode = response.data.resCode;
-                     this.swiper1Data = response.data.resData;
-                     this.resMsg = response.data.resMsg;
-                 } else {
-                     console.log(response.message);
-                 }
-             })
+        this.$api.get('api/main/pub/all', {
+        }, response =>{
+            if (response.status >= 200 && response.status < 300) {
+                console.log(response.data);
+                this.resCode = response.data.resCode;
+                this.swiper1Data = response.data.resData;
+                this.resMsg = response.data.resMsg;
+            } else {
+                console.log(response.message);
+            }
+        })
     },
     methods:{
         slideChangeTransitionEnd:function(){
